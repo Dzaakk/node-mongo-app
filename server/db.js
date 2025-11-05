385979543bffec1792a542a4969dc4fd0d16cf32
Dzkzzz <dzdzaakk@gmail.com>
2025-11-05 22:56:49 +0700

    create close db func

--- server/db.js
+++ server/db.js
@@ -6,8 +6,19 @@ let mongod;
 async function connectDB() {
     mongod = await MongoMemoryServer.create();
     const uri = mongod.getUri();
-    await mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+    await mongoose.connect(uri, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    });
     console.log('Connected to in-memory MongoDB');
+    return mongod;
 }
 
-module.exports = { connectDB };
+async function closeDB() {
+    await mongoose.connection.dropDatabase();
+    await mongoose.connection.close();
+    if (mongod) await mongod.stop();
+    console.log('In-memory MongoDB stopped');
+}
+
+module.exports = { connectDB, closeDB };
